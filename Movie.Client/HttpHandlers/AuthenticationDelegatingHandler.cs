cde1be89231ec0952e62befcab9b69e97b3f4ed7
Ebubekir ERGÜN <ebubekirergun@procenne.com>
2023-03-10 15:41:41 +0300

    Clam Based Authorization in ıdentityserver secure interactiveclient

--- Movie.Client/HttpHandlers/AuthenticationDelegatingHandler.cs
+++ Movie.Client/HttpHandlers/AuthenticationDelegatingHandler.cs
@@ -1,32 +1,46 @@
 using IdentityModel.Client;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
 
 namespace Movie.Client.HttpHandlers;
 
 public class AuthenticationDelegatingHandler : DelegatingHandler
 {
-    private readonly IHttpClientFactory _httpClientFactory;
-    private readonly ClientCredentialsTokenRequest _tokenRequest;
+    // private readonly IHttpClientFactory _httpClientFactory;
+    // private readonly ClientCredentialsTokenRequest _tokenRequest;
+    //
+    // public AuthenticationDelegatingHandler(IHttpClientFactory httpClientFactory, ClientCredentialsTokenRequest tokenRequest)
+    // {
+    //     _httpClientFactory = httpClientFactory;
+    //     _tokenRequest = tokenRequest;
+    // }
 
-    public AuthenticationDelegatingHandler(IHttpClientFactory httpClientFactory, ClientCredentialsTokenRequest tokenRequest)
+    private readonly IHttpContextAccessor _httpContextAccessor;
+
+    public AuthenticationDelegatingHandler(IHttpContextAccessor httpContextAccessor)
     {
-        _httpClientFactory = httpClientFactory;
-        _tokenRequest = tokenRequest;
+        _httpContextAccessor = httpContextAccessor;
     }
 
     protected  override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
     {
-        var httpClient = _httpClientFactory.CreateClient("IDPClient");
+        // var httpClient = _httpClientFactory.CreateClient("IDPClient");
+        //
+        // var tokenResponse = await httpClient.RequestClientCredentialsTokenAsync(_tokenRequest);
+        // if (tokenResponse.IsError)
+        // {
+        //     throw new HttpRequestException("Access Token Error");
+        // }
+        //
+        // if (tokenResponse.AccessToken != null) request.SetBearerToken(tokenResponse.AccessToken);
 
-        var tokenResponse = await httpClient.RequestClientCredentialsTokenAsync(_tokenRequest);
-        if (tokenResponse.IsError)
+        var accesToken = await _httpContextAccessor.HttpContext.GetTokenAsync(OpenIdConnectParameterNames.AccessToken);
+
+        if (!string.IsNullOrWhiteSpace(accesToken))
         {
-            throw new HttpRequestException("Access Token Error");
+            request.SetBearerToken(accesToken);
         }
-        
-        if (tokenResponse.AccessToken != null) request.SetBearerToken(tokenResponse.AccessToken);
 
-        
-        
         return await base.SendAsync(request, cancellationToken);
     }
 }
